235b5e76f04c7b506a24ee21c01a24f112a9f10a
Abinash kumar <163131540+abinashkumar19@users.noreply.github.com>
2025-11-02 12:52:01 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
+  description = "Security group ID of the EKS cluster"
+  value       = module.eks.cluster_security_group_id
 }
 
-output "node_group_arn" {
-  value = module.eks.eks_managed_node_groups["default"].node_group_arn
+output "node_group_role_arn" {
+  description = "IAM role ARN for the node group"
+  value       = module.eks.node_groups["default"].iam_role_arn
 }
